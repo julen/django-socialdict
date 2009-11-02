d54918856fc571284ffe6a97fe756517d2ef2b0f
julen <julen@deeebian.(none)>
2009-11-02 18:21:59 +0100

    Add template tag for displaying simple total statistics.

--- socialdict/templatetags/socialdict_tags.py
+++ socialdict/templatetags/socialdict_tags.py
@@ -11,8 +11,14 @@ def render_latest_socialdict_terms(num):
 @register.inclusion_tag('socialdict/alphabet_snippet.html')
 def render_socialdict_alphabet():
     alphabet = Term.objects.values_list('alphabet_letter', flat=True).distinct()
-    alphabet_list = [] 
+    alphabet_list = []
     for letter in alphabet:
         letter_count = Term.objects.filter(alphabet_letter=letter).count()
         alphabet_list.append({ 'letter': letter, 'count': letter_count })
     return { 'alphabet': alphabet_list }
+
+@register.inclusion_tag('socialdict/totals_snippet.html')
+def render_socialdict_totals():
+    term_count = Term.objects.all().count()
+    author_count = Term.objects.values('social_user').distinct().count()
+    return { 'term_count': term_count, 'author_count': author_count }
