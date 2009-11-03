749d5aad0eac4b203ff7c3422c94b13b677e4a72
Julen Ruiz Aizpuru <julenx@gmail.com>
2009-11-03 20:07:23 +0100

    Fix a bad design decision: terms are better if they're not unique, we love pluralism.

--- socialdict/models.py
+++ socialdict/models.py
@@ -4,12 +4,11 @@ from django.db import models
 from django.utils.translation import ugettext as _
 
 class Term(models.Model):
-    name = models.CharField(max_length=30, primary_key=True, unique=True,
-                            verbose_name=_("Term"))
+    name = models.CharField(max_length=50, verbose_name=_("Term"))
     meaning = models.CharField(max_length=140, verbose_name=_("Meaning"))
     status_id = models.IntegerField()
     alphabet_letter = models.CharField(max_length=1, blank=True)
-    social_user = models.CharField(max_length=30, blank=True, verbose_name=_("User"))
+    social_user = models.CharField(max_length=50, blank=True, verbose_name=_("User"))
     date_added = models.DateTimeField(default=datetime.datetime.now)
 
     def __unicode__(self):
