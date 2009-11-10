a6d79b4ce16c71ad6c2e0a4554d4568c67cebf4a
Julen Ruiz Aizpuru <julenx@gmail.com>
2009-11-10 19:42:25 +0100

    Add source field to determine where the term comes from.

--- socialdict/models.py
+++ socialdict/models.py
@@ -4,12 +4,22 @@ from django.db import models
 from django.utils.translation import ugettext as _
 
 class Term(models.Model):
+    WEB_SOURCE = 1
+    IDENTICA_SOURCE = 2
+    TWITTER_SOURCE = 3
+    SOURCE_CHOICES = (
+        (WEB_SOURCE, _('Web')),
+        (IDENTICA_SOURCE, _('Identi.ca')),
+        (TWITTER_SOURCE, _('Twitter')),
+    )
+
     name = models.CharField(max_length=50, verbose_name=_("Term"))
     meaning = models.CharField(max_length=140, verbose_name=_("Meaning"))
     status_id = models.IntegerField()
     alphabet_letter = models.CharField(max_length=1, blank=True)
     social_user = models.CharField(max_length=50, blank=True, verbose_name=_("User"))
     date_added = models.DateTimeField(default=datetime.datetime.now)
+    source = models.IntegerField(choices=SOURCE_CHOICES, default=WEB_SOURCE)
 
     def __unicode__(self):
         return self.name
