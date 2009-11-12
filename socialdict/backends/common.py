d621cc676b8ff592694e89fe2f3ffcdffc36341e
Julen Ruiz Aizpuru <julenx@gmail.com>
2009-11-12 20:37:19 +0100

    First set of backends.
    * common is a module to share code useful for backends.
    * twitter is a Twitter backend.
    
    More coming soon.

---
+++ socialdict/backends/common.py
@@ -0,0 +1,46 @@
+"""
+Common code useful for backends.
+"""
+
+from urllib2 import urlopen
+
+from django.utils import simplejson
+
+from socialdict import utils
+from socialdict.models import Term
+
+def load_json_response(url):
+    """Loads the given URL and returns the JSON response."""
+    search = urlopen(url)
+    return simplejson.loads(search.read())
+
+def add_term(text, author, id, date, source):
+    """Given the necessary data for a term, tries to check for
+       existing terms to avoid duplicates, and if none is found,
+       it is added to the database.
+    """
+    def term_exists(status_id, source, term, meaning):
+        """Checks if a definition with the given data exists."""
+        try:
+            Term.objects.get(status_id=status_id, source=source)
+            Term.objects.get(name=term, meaning=meaning)
+            return True
+        except Term.DoesNotExist:
+            return False
+
+    try:
+        term, meaning = utils.parse(text)
+        # Only add term if it doesn't exist
+        if not term_exists(id, source, term, meaning):
+            new_term = Term()
+            new_term.name = term
+            new_term.meaning = meaning
+            new_term.social_user = author
+            new_term.status_id = id
+            new_term.source = source
+            new_term.save()
+            return True, term
+        else:
+            return False, term
+    except:
+        return False, None
---
+++ socialdict/backends/twitter.py
@@ -0,0 +1,32 @@
+"""
+Twitter backend.
+"""
+
+from datetime import datetime
+
+from django.conf import settings
+
+from socialdict.backends import common
+from socialdict.models import Term
+
+SEARCH_URL = 'http://search.twitter.com/search.json?rpp=100&q=%%23%(hashtag)s' %\
+             { 'hashtag': settings.SOCIALDICT_HASHTAG }
+
+DATETIME_FORMAT = '%a, %d %b %Y %H:%M:%S +0000'
+
+def update_database_terms():
+    """Reads the Twitter API response and passes the necessary data
+       to add terms to the database.
+    """
+    status = []
+    response = common.load_json_response(SEARCH_URL)
+    for result in response['results']:
+        # Try to match a valid datetime object
+        try:
+            created = datetime.strptime(result['created_at'], DATETIME_FORMAT)
+        except ValueError:
+            created = None
+        st = common.add_term(result['text'], result['from_user'],
+                             result['id'], created, Term.TWITTER_SOURCE)
+        status.append(st)
+    return status
