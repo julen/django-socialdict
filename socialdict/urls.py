5d68ac871a14dd5f9f06fcd930904f58dea89d8e
Julen Ruiz Aizpuru <julenx@gmail.com>
2009-12-09 19:57:38 +0100

    Name views in order to reverse URLs in templates.

--- socialdict/urls.py
+++ socialdict/urls.py
@@ -9,9 +9,11 @@ urlpatterns = patterns('',
      'socialdict_term_archive_index'),
 
     (r'^(?P<letter>\w)/(?P<page>[0-9]+)/?$',
-     'socialdict.views.letter'),
+     'socialdict.views.letter', {},
+     'socialdict_letter_view_page'),
     (r'^(?P<letter>\w)/?$',
-     'socialdict.views.letter', {'page': 1}),
+     'socialdict.views.letter', {'page': 1},
+     'socialdict_letter_view'),
 
     (r'^add/?$',
      'socialdict.views.add', {},
