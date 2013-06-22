8dbcbe3aed0b528f580a68651b2b7ed302498135
Julen Ruiz Aizpuru <julenx@gmail.com>
2013-06-22 18:26:35 +0200

    Upgraded to Twitter API 1.1

--- socialdict/backends/twitter.py
+++ socialdict/backends/twitter.py
@@ -4,13 +4,25 @@ Twitter backend.
 
 from datetime import datetime
 
+import requests
+from requests_oauthlib import OAuth1
+
 from django.conf import settings
 
 from socialdict.backends import common
 
-SEARCH_URL = 'http://search.twitter.com/search.json?rpp=100&q=%%23%(hashtag)s' %\
+SEARCH_URL = 'http://search.twitter.com/search.json' %\
              { 'hashtag': settings.SOCIALDICT_HASHTAG }
-DATETIME_FORMAT = '%a, %d %b %Y %H:%M:%S +0000'
+API_BASE = 'https://api.twitter.com/1.1/'
+auth_url = API_BASE + 'account/verify_credentials.json'
+tweets_url = API_BASE + 'search/tweets.json'
+
+tweets_params = {
+    'q': '#%s' % settings.SOCIALDICT_HASHTAG,
+    'count': 100,
+}
+
+DATETIME_FORMAT = '%a %b %d %H:%M:%S +0000 %Y'
 SOURCE = 'twitter'
 
 def update_database_terms():
@@ -18,14 +30,20 @@ def update_database_terms():
        to add terms to the database.
     """
     status = []
-    response = common.load_json_response(SEARCH_URL)
-    for result in response['results']:
+
+    auth = OAuth1(settings.TWITTER_APP_KEY,
+                  settings.TWITTER_APP_SECRET,
+                  settings.TWITTER_USER_OAUTH_TOKEN,
+                  settings.TWITTER_USER_OAUTH_TOKEN_SECRET)
+    requests.get(auth_url, auth=auth)
+    response = requests.get(tweets_url, auth=auth, params=tweets_params)
+    for result in response.json()['statuses']:
         # Try to match a valid datetime object
         try:
             created = datetime.strptime(result['created_at'], DATETIME_FORMAT)
         except ValueError:
             created = None
-        st = common.add_term(result['text'], result['from_user'],
+        st = common.add_term(result['text'], result['user']['screen_name'],
                              result['id'], created, SOURCE)
         status.append(st)
     return status
